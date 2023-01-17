7937696e0afe852740b85918d1e25739fa2e2bb0
RjH <ryanjayhuffaker@gmail.com>
2023-01-17 15:38:05 -0800

    Adding cart context and shopping-bag icon

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />}></Route>
-        <Route path='/shop' element={<p>SHOPPING!!!</p>}></Route>
+        <Route path='/shop' element={<Shop />}></Route>
         <Route path='/auth' element={<Authentication />}></Route>
       </Route>
       
