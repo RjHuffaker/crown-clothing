3c322fe121d2169de454c77ea44c6825386e0066
RjH <ryanjayhuffaker@gmail.com>
2023-01-18 10:02:02 -0800

    Added addToCart functionality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,21 +1,20 @@
 import { useContext } from 'react';
 
 import { ReactComponent as ShoppingBag } from '../../assets/shopping-bag.svg';
+
 import { CartContext } from '../../contexts/cart.context';
 
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
-    const toggleIsCartOpen = () => {
-        setIsCartOpen(!isCartOpen);
-    }
-
+    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+    
     return (
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingBag className='shopping-icon' />
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
         </div>    
     );
 }
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,16 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
 import Button from '../button/button.component';
 
 import './product-card.styles.scss';
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
+
     return (
         <div className='product-card-container'>
             <img src={imageUrl} alt={`${name}`} />
@@ -11,7 +18,7 @@ const ProductCard = ({ product }) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted'>ADD TO CART</Button>
+            <Button buttonType='inverted' onClick={addProductToCart}>ADD TO CART</Button>
         </div>
     )
 }
