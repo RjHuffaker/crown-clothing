165585b36d32fcc6b69bb3338370b1c687714aae
RjH <ryanjayhuffaker@gmail.com>
2023-01-28 11:27:31 -0700

    Migrated user and categories to Redux

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,7 +1,8 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import { selectCategoriesMap } from '../../store/categories/categories.selector'
 
 import ProductCard from '../../components/product-card/product-card.component';
 
@@ -9,7 +10,9 @@ import { CategoryTitle, CategoryContainer } from './category.styles.jsx';
 
 const Category = () => {
     const { category } = useParams();
-    const { categoriesMap } = useContext(CategoriesContext);
+
+    const categoriesMap = useSelector(selectCategoriesMap);
+
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
---
+++ src/store/categories/categories.action.js
@@ -0,0 +1,5 @@
+import { CATEGORIES_ACTION_TYPES } from "./categories.types";
+import { createAction } from "../../utils/reducer/reducer.utils";
+
+export const setCategoriesMap = (categories) => 
+    createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP, categories);
